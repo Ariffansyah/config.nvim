8c40f77adc1f3b24fd5de1cff6aa05cc71283a6e
Ariffansyah <harimato9@gmail.com>
2025-02-04 19:34:55 +0700

    feat: Add Java configuration

--- init.lua
+++ init.lua
@@ -288,6 +288,7 @@ require('lazy').setup({
   'dstein64/vim-startuptime',
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  { 'mfussenegger/nvim-jdtls' },
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
@@ -2285,6 +2286,7 @@ require('lazy').setup({
         'vimdoc',
         'regex',
         'norg',
+        'java',
       },
       -- Autoinstall languages that are not installed
       auto_install = true,
