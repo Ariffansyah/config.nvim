b8a04fcff5e608826fd15607464e5c540e8541bc
Mohammad Ariffansyah <harimato9@gmail.com>
2026-03-02 05:45:25 +0700

    feat: improve screenkey popup position and auto-toggle behavior

--- lua/plugins/screenkey.lua
+++ lua/plugins/screenkey.lua
@@ -3,25 +3,46 @@ return {
   lazy = false,
   version = "*",
   config = function()
-    local width = 40 -- Define width here so we can use it in the math below
-    local height = 3
+    local width = 40
+    local height = 1 -- Single line is less likely to block things
 
     require("screenkey").setup({
       win_opts = {
         relative = "editor",
-        anchor = "SW", -- "South-West" (Bottom-Left corner of the popup)
+        anchor = "SE", -- Bottom-Right is usually safer than center
         width = width,
         height = height,
         border = "rounded",
-
-        -- Vertical Position: Bottom (with a small gap for the status line)
-        row = vim.o.lines - 2,
-
-        -- Horizontal Position: Center
-        col = (vim.o.columns - width) / 2,
+        -- Position it just above the statusline
+        row = vim.o.lines - 3,
+        col = vim.o.columns - 2,
       },
+      show_leader = true,
+      clear_after = 3,
     })
 
+    -- Start it by default
     vim.cmd("Screenkey")
+
+    -- AUTO-TOGGLE: This will close screenkey when you open "Trouble"
+    -- and reopen it when you close "Trouble"
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "trouble",
+      callback = function()
+        vim.cmd("Screenkey") -- Toggles it OFF
+
+        vim.api.nvim_create_autocmd("BufWinLeave", {
+          buffer = 0,
+          once = true,
+          callback = function()
+            vim.cmd("Screenkey") -- Toggles it back ON
+          end,
+        })
+      end,
+    })
   end,
+  keys = {
+    -- Manual toggle: hit <leader>sk if it's blocking your view!
+    { "<leader>usk", "<cmd>Screenkey<cr>", desc = "Toggle Screenkey" },
+  },
 }
